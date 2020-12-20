dafc95b67cf36c2dff9e8802a5e9c25b0077a5f6
兄 平 <597914752@qq.com>
2020-12-20 20:52:22 +0800

    update

--- ExportGradesAsPDF/README.md
+++ ExportGradesAsPDF/README.md
@@ -1,4 +1,6 @@
 # How to use
 1. Upload to `ModdleDir/grade/report/overview`
 2. Edit `index.php` on `ModdleDir/grade/report/overview`
-3. add line `echo '<script src="./export.js"></script>';` before `echo $OUTPUT->footer();`
+3. add line `echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.2.0/jspdf.umd.min.js"></script>';` before `echo $OUTPUT->footer();`
+4. add line `echo '<script src="https://unpkg.com/jspdf-autotable@3.5.13/dist/jspdf.plugin.autotable.js"></script>';` before `echo $OUTPUT->footer();`
+4. add line `echo '<script src="./export.js"></script>';` before `echo $OUTPUT->footer();`
--- ExportGradesAsPDF/export.js
+++ ExportGradesAsPDF/export.js
@@ -1,28 +1,98 @@
+var logo_url = 'https://learn.sie-gxust.com/pluginfile.php/3615/block_html/content/SIE_logo_Small.png';
+
 $(function(){
-
-    var newscript = document.createElement('script');
-    newscript.setAttribute('type','text/javascript');
-    newscript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.2.0/jspdf.umd.min.js');
-    document.body.appendChild(newscript);
-
-
-    // Add an export button to id overview-grade
-    var r=$('<input/>').attr({
+    // Add an export button to id region-main
+    var r = $('<input/>').attr({
         type: "button",
         id: "export-btn",
         value: 'Export As PDF'
     });
-    $("#overview-grade").append(r);
+    $("#region-main").append(r);
+
+    var img = $('<img>').attr({
+        id: "logo-img",
+        src: logo_url,
+        style: "display: none"
+    })
+
+    $("#region-main").append(img);
 
 
     $('#export-btn').on('click',function(){
-        // export data as PDF
-        alert('In process');
-
         // get DOMs
-        var c0 = $('.cell .c0');
-        var c1 = $('.cell .c1');
+        var c0 = $('td.c0');
+        var c1 = $('td.c1');
 
+        // get logo base64 image
 
+        var title = $('#region-main h2')[0].innerText;
+
+        // Default export is a4 paper, portrait, using millimeters for units
+        const { jsPDF } = window.jspdf;
+        const doc = new jsPDF();
+
+        try {
+            var logo = getBase64Image(document.getElementById('logo-img'))
+            doc.addImage(logo, "PNG", 90, 20, 30, 30);
+        }catch (e) {
+            alert('Incomplete or corrupt PNG file. Please try again.');
+        }
+
+        doc.setFontSize(22);
+        doc.setFont("helvetica", "bold");
+        doc.text(title, 20, 60, {
+            alignment: "center"
+        });
+
+        var headers = createHeaders([
+            'Course name',
+            'Grade'
+        ]);
+
+        var data = []
+
+        for (var cell_index = 0; cell_index < c0.length; cell_index++) {
+            if(c0[cell_index].innerText.length > 0){
+                data.push({
+                    'Course name': c0[cell_index].innerText,
+                    'Grade': c1[cell_index].innerText
+                })
+            }
+        }
+        doc.table(20, 70, data, headers, {
+            autoSize: true,
+            tableWidth: 'wrap',
+            margins: {
+                left: 1,
+                right: 1
+            },
+        });
+
+        doc.save( title + '.pdf');
     });
 });
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+function createHeaders(keys) {
+    var result = [];
+    for (var i = 0; i < keys.length; i += 1) {
+        result.push({
+            id: keys[i],
+            name: keys[i],
+            prompt: keys[i],
+            width: 65,
+            align: "center",
+            padding: 0
+        });
+    }
+    return result;
+}
